ff6c85972120d66f2a2ee25b1574b9000736495d
Paihuahua <a55877a@yahoo.com.tw>
2025-08-03 08:03:44 +0000

    1

--- lesson9/lesson9_3.py
+++ lesson9/lesson9_3.py
@@ -1,10 +1,20 @@
-#請幫我自訂一個function
-#連線至postgres DB
-#建立連線環境參數的樣版
+#請幫我建立一個function
+#傳入connection參數
+#建立一個cursor
+#執行SQL查詢
+#並回傳查詢結果
 
 #pip install psycopg2-binary
 import psycopg2
 
+
+
+def execute_query(connection, query):
+    cursor = connection.cursor()
+    cursor.execute(query)
+    result = cursor.fetchall()
+    cursor.close()
+    return result
 
 def create_connection():
     conn = psycopg2.connect(
@@ -21,12 +31,16 @@ def main():
     conn = create_connection()
     if conn:
         print("成功連接到資料庫！")
+        query = """
+        SELECT count(*) AS "筆數"
+        FROM "台鐵車站資訊";
+        """
+        result = execute_query(conn, query)
+        print("台鐵車站資訊：", result)
         conn.close()
     else:
-        print("無法連接到資料庫！")
+        print("無法連接到資料庫，請檢查設定。")
+        return
 
 if __name__ == "__main__":
     main()
-
-
-
